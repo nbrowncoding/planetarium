08965a6d5804d393696a2927c330442a203c7147
nbcoding <nick.brown.coding@gmail.com>
2022-07-04 00:29:18 -0500

    Converted planet info into a data service.

--- src/planets/planet-list.component.ts
+++ src/planets/planet-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Planet } from "./planet";
+import { PlanetData } from "./planet-data.service";
 
 @Component({
     selector: 'planet-list',
@@ -10,7 +11,10 @@ import { Planet } from "./planet";
 /**
  * Defins the the Planet List Compent that lists all of the planets in a table format
  */
-export class PlanetListComponent {
+export class PlanetListComponent implements OnInit {
+
+    // Constructor for planet data service
+    constructor(private planetDataService: PlanetData) {}
 
     // Page Title and disclaimers
     pageTitle = "List of Planets in our Solar System";
@@ -22,100 +26,14 @@ export class PlanetListComponent {
 
     // Planet data
     // TODO: Convert to json file
-    planetInfo: Planet[] = [
-        {
-            name: 'Mercury',
-            type: 'Terrestrial',
-            category: 'Planet',
-            distance: '0.4 AU',
-            imageUrl: 'assets/images/mercury.jpeg'
-        },
-        {
-            name: 'Venus',
-            type: 'Terrestrial',
-            category: 'Planet',
-            distance: '0.7 AU',
-            imageUrl: 'assets/images/venus.jpeg'
-        },
-        {
-            name: 'Earth',
-            type: 'Terrestrial',
-            category: 'Planet',
-            distance: '1 AU',
-            imageUrl: 'assets/images/earth.jpeg'
-        },
-        {
-            name: 'Mars',
-            type: 'Terrestrial',
-            category: 'Planet',
-            distance: '1.5 AU',
-            imageUrl: 'assets/images/mars.jpeg'
-        },
-        {
-            name: 'Ceres',
-            type: 'Planetoid',
-            category: 'Dwarf Planet',
-            distance: '2.8 AU',
-            imageUrl: 'assets/images/ceres.jpeg'
-        },
-        {
-            name: 'Jupiter',
-            type: 'Gas Giant',
-            category: 'Planet',
-            distance: '5.2 AU',
-            imageUrl: 'assets/images/jupiter.jpeg'
-        },
-        {
-            name: 'Saturn',
-            type: 'Gas Giant',
-            category: 'Planet',
-            distance: '9.5 AU',
-            imageUrl: 'assets/images/saturn.jpeg'
-        },
-        {
-            name: 'Uranus',
-            type: 'Ice Giant',
-            category: 'Planet',
-            distance: '19.8 AU',
-            imageUrl: 'assets/images/uranus.jpeg'
-        },
-        {
-            name: 'Neptune',
-            type: 'Ice Giant',
-            category: 'Planet',
-            distance: '30.1 AU',
-            imageUrl: 'assets/images/neptune.jpeg'
-        },
-        {
-            name: 'Pluto',
-            type: 'KBO (Kuiper belt object)',
-            category: 'Dwarf Planet',
-            distance: '39 AU',
-            imageUrl: 'assets/images/pluto.jpeg'
-        },
-        {
-            name: 'Haumea',
-            type: 'KBO (Kuiper belt object)',
-            category: 'Dwarf Planet',
-            distance: '35 to 52 AU',
-            imageUrl: 'assets/images/haumea.jpeg'
-        },
-        {
-            name: 'Makemake',
-            type: 'KBO (Kuiper belt object)',
-            category: 'Dwarf Planet',
-            distance: '39 to 53 AU',
-            imageUrl: 'assets/images/makemake.jpeg'
-        },
-        {
-            name: 'Eris',
-            type: 'KBO (Kuiper belt object)',
-            category: 'Dwarf Planet',
-            distance: '38 to 68 AU',
-            imageUrl: 'assets/images/eris.jpeg'
-        }
-    ];
+    planetInfo: Planet[] = [];
 
     // Planet information that is displayed on the table
     displayedColumns: string[] = ['imageUrl', 'name', 'type', 'category', 'distance'];
+
+    // Configure initialization
+    ngOnInit(): void {
+        // Get the planet data from the service when component initializes
+        this.planetInfo = this.planetDataService.getPlanetData();
+    }
 }
